3809d72f40680c0baca84e0936fb9760d7785029
Alan Mong <alanmongx@gmail.com>
2024-08-24 17:26:06 -0700

    remove key

--- config/key.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  googleClientID:
-    "267854164521-abkcghqqub32n1v5hn2rlgeop6uo0ecf.apps.googleusercontent.com",
-  googleClientSecret: "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",
-};
