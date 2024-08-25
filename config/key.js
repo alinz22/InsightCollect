f3a8d474f935e1c55d33ccb4488ee665b42c6f42
Alan Mong <alanmongx@gmail.com>
2024-08-24 17:22:54 -0700

    add google auth api

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "267854164521-abkcghqqub32n1v5hn2rlgeop6uo0ecf.apps.googleusercontent.com",
+  googleClientSecret: "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",
+};
