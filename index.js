5278c7105442c040d463584c0c7d29c984f05f46
Alan Mong <alanmongx@gmail.com>
2024-08-25 19:46:19 -0700

    create user model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("./services/passport");
 const keys = require("./config/keys");
 const authRoutes = require("./routes/authRoutes");
+require("./models/User");"
 
 mongoose.connect(keys.mongoURI);
 
