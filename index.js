16457a5d8f2621d6bc6d23ce2daef09d0b33b161
Alan Mong <alanmongx@gmail.com>
2024-08-25 19:37:29 -0700

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
+const keys = require("./config/keys");
 const authRoutes = require("./routes/authRoutes");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
