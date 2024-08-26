95472d3b3480f890bf56100941d7a97d06b5407e
Alan Mong <alanmongx@gmail.com>
2024-08-25 19:59:07 -0700

    save users to DB

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("./services/passport");
 const keys = require("./config/keys");
 const authRoutes = require("./routes/authRoutes");
-const User = require("./models/User");
+require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
