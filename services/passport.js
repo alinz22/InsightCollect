ba2820143b7ff4dafcaa13ac05a4aebc5fab019d
Alan Mong <alanmongx@gmail.com>
2024-08-27 23:37:11 -0700

    add middlware for processing cookies

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,11 +23,21 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value,
-      }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+          })
+            .save()
+            .then((user) => {
+              done(null, user);
+            });
+        }
+      });
     }
   )
 );
