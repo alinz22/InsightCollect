95f786762e183f94b1f82a4ec2a061fe41502ede
Alan Mong <alanmongx@gmail.com>
2024-09-11 23:57:07 -0700

    header compoent and css and routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+
+const Dashboard = () => {
+  <h2>D</h2>;
+};
+const SurveyNew = () => {
+  <h2>SN</h2>;
+};
+const Landing = () => {
+  <h2>L</h2>;
+};
 
 const App = () => {
   return (
     <>
       <div>
-        <h1>Hello, React!</h1>
-        <p>This is a simple React application.</p>
+        <BrowserRouter>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
+        </BrowserRouter>
       </div>
     </>
   );
